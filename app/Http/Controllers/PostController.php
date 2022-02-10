15141f620cbe276c27bbaf1cfbb5a00c2da33a87
Komiya Takashi <t_komiya@uluru859.local>
2022-02-10 10:03:46 +0900

    PostController Remake

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,6 +6,17 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
