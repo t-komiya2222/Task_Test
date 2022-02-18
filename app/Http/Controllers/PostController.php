7d06f7c56ba7ced2aeaa2fededcb151f60613846
Komiya Takashi <t_komiya@uluru859.local>
2022-02-18 10:44:00 +0900

    [clean] use /Exception → use Exception

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
-use \Exception;
+use Exception;
 
 class PostController extends Controller
 {
